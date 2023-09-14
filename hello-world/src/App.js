cf8de28160fbf0dc9f1c2f8f1fc9f85a4ffae69d
Razi Ullah <raziullah913@gmail.com>
2023-09-15 00:07:10 +0500

    episode 2 practice session

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -22,6 +22,7 @@ function App() {
       <Breaker />
 
       {/* calling the "User" component */}
+      {/* prints nothing because nothing is passed */}
       <User />
 
       <Breaker />
@@ -30,6 +31,9 @@ function App() {
       <User name="Razi" age={22} email="razi@razi.com"/>
       <Breaker />
       <User name="Jake" age={29} email="jake@razi.com"/>
+
+      <Breaker />
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
     </div>
   );
 }
@@ -59,9 +63,19 @@ const GetNameComponent = () => {
 const User = (props) => {
   return (
     <div>
-      <h1> props.name</h1>
-      <h1> props.age</h1>
-      <h1> props.email</h1>
+      <h1> {props.name}</h1>
+      <h1> {props.age}</h1>
+      <h1> {props.email}</h1>
+    </div>
+  );
+};
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1> {props.salary}</h1>
+      <h1> {props.position}</h1>
+      <h1> {props.company}</h1>
     </div>
   );
 };
